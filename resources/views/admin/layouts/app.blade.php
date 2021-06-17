b57afcf65758b3187656597a9c6dd264b5a03505
隋文帝 Sui Wendi <suiwendi1024@163.com>
2021-06-17 12:39:48 +0800

    优化：登录后跳转登录前访问的页面，登录页面自动填入测试用户账号密码，增加本地化文件

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -33,6 +33,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        {{-- 博客 --}}
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('posts.index') }}">博客</a>
+                        </li>
+                        {{-- 商店 --}}
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('products.index') }}">商店</a>
+                        </li>
 
                     </ul>
 
